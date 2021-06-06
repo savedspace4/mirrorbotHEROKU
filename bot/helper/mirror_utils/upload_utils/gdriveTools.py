b14ed6300414b1df0b3ee2b36d4e25ad4f1b2db9
savedspace4 <76652825+savedspace4@users.noreply.github.com>
2021-06-06 16:11:02 +0530

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -322,16 +322,16 @@ class GoogleDriveHelper:
                 buttons = button_build.ButtonMaker()
                 if SHORTENER is not None and SHORTENER_API is not None:
                     surl = requests.get('https://{}/api?api={}&url={}&format=text'.format(SHORTENER, SHORTENER_API, durl)).text
-                    buttons.buildbutton("🌍 𝗚-𝗗𝗥𝗜𝗩𝗘 𝗟𝗜𝗡𝗞 🌍", surl)
+                    buttons.buildbutton("😍 GDrive Link 😍", surl)
                 else:
-                    buttons.buildbutton("🌍 𝗚-𝗗𝗥𝗜𝗩𝗘 𝗟𝗜𝗡𝗞 🌍", durl)
+                    buttons.buildbutton("😍 GDrive Link 😍", durl)
                 if INDEX_URL is not None:
                     url = requests.utils.requote_uri(f'{INDEX_URL}/{meta.get("name")}/')
                     if SHORTENER is not None and SHORTENER_API is not None:
                         siurl = requests.get('https://{}/api?api={}&url={}&format=text'.format(SHORTENER, SHORTENER_API, url)).text
-                        buttons.buildbutton("🔒 𝗜𝗡𝗗𝗘𝗫 𝗟𝗜𝗡𝗞 🔒", siurl)
+                        buttons.buildbutton("😘 Index Link 😘", siurl)
                     else:
-                        buttons.buildbutton("🔒 𝗜𝗡𝗗𝗘𝗫 𝗟𝗜𝗡𝗞 🔒", url)
+                        buttons.buildbutton("😘 Index Link 😘", url)
                 if BUTTON_THREE_NAME is not None and BUTTON_THREE_URL is not None:
                     buttons.buildbutton(f"{BUTTON_THREE_NAME}", f"{BUTTON_THREE_URL}")
                 if BUTTON_FOUR_NAME is not None and BUTTON_FOUR_URL is not None:
@@ -345,9 +345,9 @@ class GoogleDriveHelper:
                 buttons = button_build.ButtonMaker()
                 if SHORTENER is not None and SHORTENER_API is not None:
                     surl = requests.get('https://{}/api?api={}&url={}&format=text'.format(SHORTENER, SHORTENER_API, durl)).text
-                    buttons.buildbutton("🌍 𝗚-𝗗𝗥𝗜𝗩𝗘 𝗟𝗜𝗡𝗞 🌍", surl)
+                    buttons.buildbutton("😍 GDrive Link 😍", surl)
                 else:
-                    buttons.buildbutton("🌍 𝗚-𝗗𝗥𝗜𝗩𝗘 𝗟𝗜𝗡𝗞 🌍", durl)
+                    buttons.buildbutton("😍 GDrive Link 😍", durl)
                 try:
                     msg += f'\n\n<b>➩ 💾 Total Size :</b> <code>{get_readable_file_size(int(meta.get("size")))}</code>\n\n<b>#File Cloned To Team Drive ✅</b>\n\n<b>➩ 🗳 𝗣𝗼𝘄𝗲𝗿𝗲𝗱 𝗕𝘆 @AT_BOTS</b>\n\n<b>⚠ 𝗗𝗢 𝗡𝗢𝗧 <u>𝗦𝗛𝗔𝗥𝗘</u> 𝗜𝗡𝗗𝗘𝗫 𝗟𝗜𝗡𝗞 𝗣𝗨𝗕𝗟𝗜𝗖𝗟𝗬 ⚠</b>'
                 except TypeError:
@@ -356,9 +356,9 @@ class GoogleDriveHelper:
                     url = requests.utils.requote_uri(f'{INDEX_URL}/{file.get("name")}')
                     if SHORTENER is not None and SHORTENER_API is not None:
                         siurl = requests.get('https://{}/api?api={}&url={}&format=text'.format(SHORTENER, SHORTENER_API, url)).text
-                        buttons.buildbutton("🔒 𝗜𝗡𝗗𝗘𝗫 𝗟𝗜𝗡𝗞 🔒", siurl)
+                        buttons.buildbutton("😘 Index Link 😘", siurl)
                     else:
-                        buttons.buildbutton("🔒 𝗜𝗡𝗗𝗘𝗫 𝗟𝗜𝗡𝗞 🔒", url)
+                        buttons.buildbutton("😘 Index Link 😘", url)
                 if BUTTON_THREE_NAME is not None and BUTTON_THREE_URL is not None:
                     buttons.buildbutton(f"{BUTTON_THREE_NAME}", f"{BUTTON_THREE_URL}")
                 if BUTTON_FOUR_NAME is not None and BUTTON_FOUR_URL is not None:
@@ -514,36 +514,36 @@ class GoogleDriveHelper:
                         if SHORTENER is not None and SHORTENER_API is not None:
                             sfurl = requests.get(
                                 'https://{}/api?api={}&url={}&format=text'.format(SHORTENER, SHORTENER_API, furl)).text
-                            msg += f"<b><a href={sfurl}>🌠 𝗚-𝗗𝗥𝗜𝗩𝗘 𝗟𝗜𝗡𝗞 🌠</a></b>"
+                            msg += f"<b><a href={sfurl}>😍 GDrive Link 😍</a></b>"
                         else:
-                            msg += f"<b><a href={furl}>🌠 𝗚-𝗗𝗥𝗜𝗩𝗘 𝗟𝗜𝗡𝗞 🌠</a></b>"
+                            msg += f"<b><a href={furl}>😍 GDrive Link 😍</a></b>"
                         if INDEX_URL is not None:
                             url = requests.utils.requote_uri(f'{INDEX_URL}/{file.get("name")}/')
                             if SHORTENER is not None and SHORTENER_API is not None:
                                 siurl = requests.get(
                                     'https://{}/api?api={}&url={}&format=text'.format(SHORTENER, SHORTENER_API,
                                                                                       url)).text
-                                msg += f' <b>| <a href="{siurl}">☄️ 𝗜𝗡𝗗𝗘𝗫 𝗟𝗜𝗡𝗞 ☄️</a></b>'
+                                msg += f' <b>| <a href="{siurl}">😘 Index Link 😘</a></b>'
                             else:
-                                msg += f' <b>| <a href="{url}">☄️ 𝗜𝗡𝗗𝗘𝗫 𝗟𝗜𝗡𝗞 ☄️</a></b>'
+                                msg += f' <b>| <a href="{url}">😘 Index Link 😘</a></b>'
                     else:
                         furl = f"https://drive.google.com/uc?id={file.get('id')}&export=download"
                         msg += f"⁍<code>{file.get('name')}<br>({get_readable_file_size(int(file.get('size')))})📄</code><br>"
                         if SHORTENER is not None and SHORTENER_API is not None:
                             sfurl = requests.get(
                                 'https://{}/api?api={}&url={}&format=text'.format(SHORTENER, SHORTENER_API, furl)).text
-                            msg += f"<b><a href={sfurl}>🌠 𝗚-𝗗𝗥𝗜𝗩𝗘 𝗟𝗜𝗡𝗞 🌠</a></b>"
+                            msg += f"<b><a href={sfurl}>😍 GDrive Link 😍</a></b>"
                         else:
-                            msg += f"<b><a href={furl}>🌠 𝗚-𝗗𝗥𝗜𝗩𝗘 𝗟𝗜𝗡𝗞 🌠</a></b>"
+                            msg += f"<b><a href={furl}>😍 GDrive Link 😍</a></b>"
                         if INDEX_URL is not None:
                             url = requests.utils.requote_uri(f'{INDEX_URL}/{file.get("name")}')
                             if SHORTENER is not None and SHORTENER_API is not None:
                                 siurl = requests.get(
                                     'https://{}/api?api={}&url={}&format=text'.format(SHORTENER, SHORTENER_API,
                                                                                       url)).text
-                                msg += f' <b>| <a href="{siurl}">☄️ 𝗜𝗡𝗗𝗘𝗫 𝗟𝗜𝗡𝗞 ☄️</a></b>'
+                                msg += f' <b>| <a href="{siurl}">😘 Index Link 😘</a></b>'
                             else:
-                                msg += f' <b>| <a href="{url}">☄️ 𝗜𝗡𝗗𝗘𝗫 𝗟𝗜𝗡𝗞 ☄️</a></b>'
+                                msg += f' <b>| <a href="{url}">😘 Index Link 😘</a></b>'
                     msg += '<br><br>'
                     content_count += 1
                     if content_count == TELEGRAPHLIMIT:
